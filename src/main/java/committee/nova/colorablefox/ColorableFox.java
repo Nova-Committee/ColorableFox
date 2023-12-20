c2df622f44bde9affbfa468c534c0972f07c312f
MikhailTapio <mikhailtapio13468@163.com>
2023-12-20 13:32:14 +0800

    Port to Forge 1.18.2

---
+++ src/main/java/committee/nova/colorablefox/ColorableFox.java
@@ -0,0 +1,14 @@
+package committee.nova.colorablefox;
+
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod("colorablefox")
+public class ColorableFox {
+    public ColorableFox() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
